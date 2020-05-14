425e225e2838c4b438926109cca4e45ecd3e2400
Braden <fish0859@gmail.com>
2020-05-14 10:57:34 -0600

    recipes formula done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,21 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    max = math.inf
+    for ingName in recipe:
+        if ingName not in ingredients:
+            max = 0
+        elif ingredients[ingName] / recipe[ingName] < max:
+            max = ingredients[ingName] / recipe[ingName]
+    return math.floor(max)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 20, 'butter': 30, 'flour': 50}
+    ingredients = {'milk': 400, 'butter': 150, 'flour': 102}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
